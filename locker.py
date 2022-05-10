a2058c9f7169858be79df00cd8f93cdf84d663f2
xXLarryTFVWXx <xxlarrytfvwxx@gmail.com>
2022-05-10 01:02:47 -0800

    The ValueError is known

--- locker.py
+++ locker.py
@@ -1,9 +1,9 @@
 import os, sys, shutil, base64, hashlib, cryptography, zipfile
 
+from cryptography import fernet
+
 if not sys.platform == "win32":
     raise SystemExit("This program is only for Windows.")
-
-
 
 def lock(source, username, password, locker_mode=0):
     """
@@ -17,10 +17,10 @@ def lock(source, username, password, locker_mode=0):
         else:
             with open(source, "rb") as f:
                 data = f.read()
-            fernet_object = cryptography.fernet.Fernet(base64.urlsafe_b64encode(hashlib.sha256(password.encode()).hexdigest()))
+            fernet_object = cryptography.fernet.Fernet(f"{base64.urlsafe_b64encode(hashlib.sha256(password.encode()).hexdigest().encode())}".encode())
             encrypted_password = fernet_object.encrypt(password.encode())
             encrypted_data = fernet_object.encrypt(data)
-            encrypted_uname = fernet_object.encrypt(username.encode())
+            encrypted_uname = base64.urlsafe_b64encode(fernet_object.encrypt(username.encode()))
             with open("result.txt", "w") as dummy:
                 dummy.write("1")
             with open(f"result.txt:{encrypted_uname}", "wb") as f:
@@ -46,13 +46,13 @@ def lock(source, username, password, locker_mode=0):
                 dummy.write("1")
             with open(f"result.txt:{encrypted_uname}", "wb") as f:
                 f.write(encrypted_password)
-            with open(f"result.txt:{encrypted_uname}:F", "wb") as f:
+            with open(f"result.txt:{encrypted_uname}:D", "wb") as f:
                 f.write(encrypted_data)
             shutil.rmtree(source)
             os.remove("result.zip")
 
 def unlock(username, password, locker_mode=0): # 0 = file, 1 = directory
-    fernet_object = cryptography.fernet.Fernet(base64.urlsafe_b64encode(hashlib.sha256(password.encode()).hexdigest()))
+    fernet_object = cryptography.fernet.Fernet(f"{base64.urlsafe_b64encode(hashlib.sha256(password.encode()).hexdigest())}".encode())
     encrypted_uname = base64.urlsafe_b64encode(fernet_object.encrypt(username.encode()))
     try:
         with open(f"result.txt:{encrypted_uname}", "rb") as f:
@@ -62,12 +62,40 @@ def unlock(username, password, locker_mode=0): # 0 = file, 1 = directory
         raise SystemExit("unable to open file/folder.")
     except cryptography.fernet.InvalidToken:
         raise SystemExit("Unable to open file/folder.")
+    if not os.path.isfile("result.txt"):
+        raise SystemExit("Unable to open file/folder.")
+    with open("result.txt", "r") as f:
+        if f.read() == "1":
+            with open(f"result.txt:{encrypted_uname}", "rb") as f:
+                encrypted_password = f.read()
+    if fernet_object.decrypt(encrypted_password) == password.encode():
+        with open(f"result.txt:{encrypted_uname}:F", "rb") as f:
+            encrypted_data = f.read()
+            #decrypt data
+            decrypted_data = fernet_object.decrypt(encrypted_data)
+            #get the file name from the decrypted data
+            decrypted_zipfile = zipfile.ZipFile(decrypted_data)
+            decrypted_zipfile.extractall(decrypted_zipfile.namelist()[0])
+            decrypted_zipfile.close()
 
-    if locker_mode == 0:
-        if not os.path.isfile("result.txt"):
-            raise SystemExit("Unable to open file/folder.")
+if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        if sys.argv[1] == "lock":
+            source = input("Enter the source file/folder: ")
+            username = input("Enter the username: ")
+            password = input("Enter the password: ")
+            if os.path.isfile(source):
+                locker_mode = 0
+            elif os.path.isdir(source):
+                locker_mode = 1
+            else:
+                raise SystemExit("Source is not a file or directory.")
+            lock(source, username, password, locker_mode)
+        elif sys.argv[1] == "unlock":
+            username = input("Enter the username: ")
+            password = input("Enter the password: ")
+            unlock(username, password)
         else:
-            with open("result.txt", "r") as f:
-                if f.read() == "1":
-                    with open(f"result.txt:{encrypted_uname}", "rb") as f:
-                        encrypted_password = f.read()
+            raise SystemExit("Invalid command.")
+    else:
+        raise SystemExit("Invalid number of arguments.")
